e7a6b950c2277d40d9118d80554daf8867edd0e5
Romain <RomainFrechic@outlook.fr>
2017-01-13 15:39:32 +0100

    Combien de mots font exactement 15 caractères dans le dictionnaire

--- index.php
+++ index.php
@@ -5,16 +5,28 @@
 	<title>PHP CRUNCHING</title>
 </head>
 <body>
-<?php 
-$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE__PATH);
+	<?php 
+	$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE__PATH);
 
-$dico = explode("\n", $string);
+	$dico = explode("\n", $string);
 
-foreach ($dico as $key => $value) {
-	echo $value;
-};
+	$countElementArray = count($dico);
+
+	echo "Ce Dictionnaire contient: ".$countElementArray." mots";
 
 
- ?> 	
+//Combien de mots font exactement 15 caractères dans le dictionnaire	
+	function longeurCaractere($dico){
+		$val=0;
+	foreach ($dico as $key => $value ) {
+      if(strlen($value) === 15){
+      	$val++;
+      }
+     } 
+     echo strval($val);
+	}
+		longeurCaractere($dico);
+	?> 	
+
 </body>
 </html>
