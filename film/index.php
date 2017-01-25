0a0703b7a367304794ab032168e43afdec0ebc78
Romain <RomainFrechic@outlook.fr>
2017-01-25 11:44:39 +0100

    le plus vieurx et le plus recent

--- film/index.php
+++ film/index.php
@@ -23,25 +23,60 @@ for($i=0;$i<11;$i++ ){
 
 //Classement du film Gravity
 
-	foreach ($top as $key => $value){
-		$film= $value['im:name']['label'];
-		$expl = explode(" - ", $film);
-		$classement = $key + 1;
-		if($expl[0] === "Gravity"){
-			echo 'le classement du film Gravity est : '.$classement.'</br>';
-		}
+foreach ($top as $key => $value){
+	$film= $value['im:name']['label'];
+	$expl = explode(" - ", $film);
+	$classement = $key + 1;
+	if($expl[0] === "Gravity"){
+		echo 'le classement du film Gravity est : '.$classement.'</br>';
 	}
+}
 
 
 // Les Réalisateur de Lego Movie 
-	foreach ($top as $key => $value) {
-		$film = $value["title"]["label"];
-		$expl = explode(" - ", $film);
-		$classement = $key + 1;
-		if ($expl[0] === "The LEGO Movie") {
-			echo  'Le/les Résalisateurs de The LEGO Movie sont : '. $expl[1].'</br>';
-		}
+foreach ($top as $key => $value) {
+	$film = $value["title"]["label"];
+	$expl = explode(" - ", $film);
+	if ($expl[0] === "The LEGO Movie") {
+		echo  'Le/les Résalisateurs de The LEGO Movie sont : '. $expl[1].'</br>';
 	}
+}
+
+// Combien de films sont sortis avant 2000
+foreach ($top as $key => $value) {
+	$film = $value["im:releaseDate"]["label"];
+	$timeCurrent = strtotime($film);
+	if($timeCurrent < 2000){
+		$valueTime++;
+		$valueTime;
+	}
+	
+} 
+
+// Quel est le film le plus récent ? Le plus vieux 
+foreach ($top as $key => $value) {
+	$titleRecent = $value["title"]["label"];
+	$expl = explode(" - ", $titleRecent);
+	$titleDate=$expl[0];
+	$filmDate = $value["im:releaseDate"]["label"];
+	$arrDate[$filmDate] = $titleDate;
+} 
+krsort($arrDate);
+$filmReset = reset($arrDate);
+$filmEnd = end($arrDate);
+
+echo 'Le film le plus récent est : '.$filmReset.'</br>';
+echo 'Le film le plus vieux est : '.$filmEnd.'</br>';	
+
+
+
+
+// Quelle est la catégorie de films la plus représentée 
+// Quel est le réalisateur le plus présent dans le top100 
+// Combien cela coûterait-il d'acheter le top10 sur iTunes/de le louer 
+// Quel est le mois ayant vu le plus de sorties au cinéma 
+// Quels sont les 10 meilleurs films à voir en ayant un budget limité 
+
 ?>
 </body>
 </html>
