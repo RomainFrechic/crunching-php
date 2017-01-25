983c16a268bbbc4410bcf9b196878e361c0b6f91
Romain <RomainFrechic@outlook.fr>
2017-01-25 10:03:22 +0100

    Gravity est Lego moovie

--- film/index.php
+++ film/index.php
@@ -9,20 +9,39 @@
 
 //tTélécharger le top100 des films visionnés au États-Unis sur la plateforme iTunes.
 	$string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
-$brut = json_decode($string, true);
+	$brut = json_decode($string, true);
 $top = $brut["feed"]["entry"]; # liste de films
 
 //Combien de film contient le document film.json.
-	$countElementArray = count($top);
-	echo "Le nombre de film: ".$countElementArray."</br>";
+$countElementArray = count($top);
+echo "Le nombre de film: ".$countElementArray."</br>";
 
 //Afficher le top10 des films
-	for($i=0;$i<11;$i++ ){
-		echo $top[$i]['im:name']['label'].'</br>';
+for($i=0;$i<11;$i++ ){
+	echo $top[$i]['im:name']['label'].'</br>';
+}
+
+//Classement du film Gravity
+
+	foreach ($top as $key => $value){
+		$film= $value['im:name']['label'];
+		$expl = explode(" - ", $film);
+		$classement = $key + 1;
+		if($expl[0] === "Gravity"){
+			echo 'le classement du film Gravity est : '.$classement.'</br>';
+		}
 	}
 
 
-   
+// Les Réalisateur de Lego Movie 
+	foreach ($top as $key => $value) {
+		$film = $value["title"]["label"];
+		$expl = explode(" - ", $film);
+		$classement = $key + 1;
+		if ($expl[0] === "The LEGO Movie") {
+			echo  'Le/les Résalisateurs de The LEGO Movie sont : '. $expl[1].'</br>';
+		}
+	}
 ?>
 </body>
 </html>
